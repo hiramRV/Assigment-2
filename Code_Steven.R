25ea5ea1191bccd8df93405e295ae382d23dcecf
HiramRubio <stevenrv97@gmail.com>
2024-12-27 11:47:37 +0100

    Update
    I created the template for the main script and my own.

---
+++ Code_Steven.R
@@ -0,0 +1,17 @@
+#################
+## Main code for Assignment 2
+## Steven Rubio
+#################
+
+
+### Part 1: ----
+
+### Part 2: ----
+
+### Part 3: ----
+
+### Part 4: ----
+
+### Part 5: ----
+
+### Part 6: ----
---
+++ Main.R
@@ -0,0 +1,20 @@
+#################
+## Main code for Assignment 2
+## Integrates:
+## Caroline
+## Faezeh
+## Pablo
+## Steven
+#################
+
+### Part 1: ----
+
+### Part 2: ----
+
+### Part 3: ----
+
+### Part 4: ----
+
+### Part 5: ----
+
+### Part 6: ----
