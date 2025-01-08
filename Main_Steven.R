821bec19ab539713763d9c1b6368ef665c68439d
hiramRV <stevenrv97@gmail.com>
2025-01-09 00:03:43 +0100

    Update
    Task 2 done. Same result by hand and with R.

--- Main_Steven.R
+++ Main_Steven.R
@@ -18,6 +18,7 @@ data <-read.xlsx("dataset01.xlsx")
 
 ### Pre-processing ----
 str(data)
+View(data)
 data$REGION<-factor(data$REGION)
 data$TYPE<-factor(data$TYPE)
 data$BALCONY<-factor(data$BALCONY)
@@ -47,12 +48,27 @@ print(paste0("p-value by hand of: ",p_value))
 
 ### Part 2: ----
 hist(data$AREA,breaks=30)
+n=length(data$AREA)
+#We don't care about the distribution.
 #Significance level of 
 #Size of the houses in the region is 75m^2
 #Significance of 5% means
 qt(p=0.025, df=n-1) #-1.96  
-ev=75
-t.test(x=data$AREA,alternative="two.sided",mu=ev,conf.level = 0.95)
+mu_2=75
+#Using R
+test2=t.test(x=data$AREA,alternative="two.sided",mu=ev,conf.level = 0.95)
+print(paste0("With a P-value of: ",round(test2$p.value,4),". We reject the null with a conf level of 95%"))
+
+#By Hand Calculations
+x_obs = mean(data$AREA)
+print(paste0("Observed price: ",x_obs))
+s = sqrt(sum((data$AREA - mu_2)^2/(n - 1))) 
+se_2 = s/sqrt(n)
+t_obs2 = (x_obs-mu_2)/se_2
+print(paste0("Observed t-value: ",t_obs))  #2.839
+p_value2 = 2*(1-pt(q=t_obs2,df=n-1))       #Calculated using t-test.
+print(paste0("p-value with R of: ",test2$p.value)) 
+print(paste0("p-value by hand of: ",p_value2)) 
 
 ### Part 3: ----
 
